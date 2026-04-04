f1d1bcb3b92dd8ecd48c080362647c082d7e748a
BarkinKctp <barkinkocatepe12@gmail.com>
2026-04-04 19:50:27 +0300

    refactor Project component styles for consistency and responsiveness

--- components/project.tsx
+++ components/project.tsx
@@ -32,12 +32,12 @@ export default function Project({
       <section
         className=" bg-gray-100 dark:bg-slate-900 max-w-[42rem] relative
     border border-black/60 dark:border-white/60 overflow-hidden sm:pr-8 rounded-lg
-    sm:h-[20rem] group-even:pl-8 hover:bg-gray-200 dark:hover:bg-slate-800 transition"
+    sm:h-[20rem] sm:group-even:pl-8 hover:bg-gray-200 dark:hover:bg-slate-800 transition"
       >
         <div
           className="pt-4 pb-7 px-5 sm:pl-10 sm:pt-10
       sm:pr-2 sm:max-w-1/2 flex flex-col h-full
-       group-even:ml-[18rem] "
+       sm:group-even:ml-[18rem] "
         >
           <h3 className="text-2xl font-semibold text-gray-700 dark:text-slate-100 hover:text-cyan-600">
             {title}
@@ -62,17 +62,17 @@ export default function Project({
           width={450}
           height={300}
           alt="Projects that I worked on"
-          className="absolute hidden sm:block top-13 -right-40 
-          w-[28.25rem] rounded-t-lg shadow-2xl transition 
+          className="sm:absolute top-13 sm:-right-40 
+          w-full sm:w-[28.25rem] rounded-t-lg shadow-2xl transition 
         group-hover:scale-[1.04] object-cover object-center
-        group-hover:-translate-x-3
-        group-hover:translate-y-3
-        group-hover:-rotate-2
+        sm:group-hover:-translate-x-3
+        sm:group-hover:translate-y-3
+        sm:group-hover:-rotate-2
 
-        group-even:group-hover:translate-x-3
-        group-even:group-hover:translate-y-3
-        group-even:group-hover:rotate-2
-        group-even:right-[initial] group-even:-left-40"
+        sm:group-even:group-hover:translate-x-3
+        sm:group-even:group-hover:translate-y-3
+        sm:group-even:group-hover:rotate-2
+        sm:group-even:right-[initial] sm:group-even:-left-40"
         />
       </section>
     </motion.div>
