bf59426073d2807657032e0520acef04c3d44e6e
BarkinKctp <barkinkocatepe12@gmail.com>
2026-05-03 23:23:24 +0300

    Remove unused getErrorMessage function from utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,19 +11,3 @@ const OBJECT_ID_RE = /^[0-9a-fA-F]{24}$/;
 export const isValidObjectId = (id: unknown): id is string => {
   return typeof id === "string" && OBJECT_ID_RE.test(id);
 };
-
-export const getErrorMessage = (error: unknown): string => {
-  let message: string;
-
-  if (error instanceof Error) {
-    message = error.message;
-  } else if (error && typeof error === "object" && "message" in error) {
-    message = String(error.message);
-  } else if (typeof error === "string") {
-    message = error;
-  } else {
-    message = "Something went wrong";
-  }
-
-  return message;
-};
